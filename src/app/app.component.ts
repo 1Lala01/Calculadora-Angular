815f1207fc74684359e6bd602e7c9958bc9fd9fe
Lorena <130251425+1Lala01@users.noreply.github.com>
2024-02-20 22:07:31 -0500

    realize una calduladora con angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'calculadora_Angular';
+  title = 'Mini Calculadora';
+  
+  num1:number= 0;
+  num2:number= 0;
+  resultado:number= 0;
+
+  suma():void{
+    this.resultado =  this.num1 + this.num2;
+  }
+  resta():void{
+    this.resultado = this.num1 - this.num2;
+  }
+  divicion():void{
+    this.resultado = this.num1 / this.num2;
+  }
+  multiplicar():void{
+    this.resultado = this.num1 * this.num2;
+  }
+
 }
